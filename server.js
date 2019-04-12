07d285d0c4e618c62cbedeee7c7a153ed5b950b4
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-12 08:48:40 -0700

    refactor server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require('http');
 
-http.createServer(function(request, response) {
+function onRequest(request, response) {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
   response.write('Hello World');
   response.end();
-}).listen(7890);
+}
+
+
+http.createServer(onRequest).listen(7890);
