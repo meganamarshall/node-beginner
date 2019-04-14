20dff8d52bae10282683016ebb5d2ad6720ed80a
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-14 11:55:42 -0700

    adding handler

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const http = require('http');
 const url = require('url');
 const router = require('./router');
 
-function start() {
+function start(route, handle) {
   function onRequest(request, response) {
     const pathname = url.parse(request.url).pathname;
     console.log('request for' + pathname + 'received');
 
-    router.route(pathname);
+    router.route(handle, pathname);
 
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write('Hello World');
