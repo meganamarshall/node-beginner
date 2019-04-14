b43dbfcfaee33e7484a37461e89036f222b7ae40
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-14 12:02:19 -0700

    what not 2do(will run into issues w/ non-blcking ops

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,11 @@
 function start() {
   console.log('request handler "start" was called');
+  return 'hello start';
 }
 
 function upload() {
   console.log('request handler "upload" was called');
+  return 'hello upload';
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -7,10 +7,11 @@ function start(route, handle) {
     const pathname = url.parse(request.url).pathname;
     console.log('request for' + pathname + 'received');
 
-    router.route(handle, pathname);
+    // router.route(handle, pathname);
 
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello World');
+    const content = route(handle, pathname);
+    response.write(content);
     response.end();
   }
   
